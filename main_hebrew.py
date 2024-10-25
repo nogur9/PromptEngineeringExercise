52ca936b1620caf89f631e9824faccdc0a3c62e6
nogur <nogur9@gmail.com>
2024-10-25 10:05:53 +0300

    fixed Hebrew version

--- main_hebrew.py
+++ main_hebrew.py
@@ -6,14 +6,15 @@ import re
 openai.api_key = "Your_key"
 
 
-st.title("אפליקציה ללימוד בינה מלאכותית")
+st.title("אפליקציה ללימוד שימוש בבינה מלאכותית")
 
 
-def create_image(prompt:str):
-
+def create_image(prompt: str):
+    fixed_prompt = prompt[::-1]
+    print(f"{fixed_prompt = }")
     # Generate the image
     response = openai.Image.create(
-        prompt=prompt,
+        prompt=fixed_prompt,
         n=1,  # Number of images to generate
         size="1024x1024"  # Image size, can be 256x256, 512x512, or 1024x1024
     )
@@ -44,26 +45,50 @@ def evaluate_prompt_with_heuristics(prompt):
 
 
 def get_cat_breeds_list(text: str):
-    regex = r'cat_breeds = (\[.*?\])'
+    regex = r'(\[.*?\])'
 
     extraction_prompt = f"""
-        Hey :)
-        Please extract as a python list, the names of cat breeds from the next sentence:
+        היי :)
+        בבקשה תוציא את רשימת גזעי החתולים מתוך הטקסט הבא:
         {text}\n\n
 
 
-        For example,
-        if sentence = "The Nebelung is a pedigree breed of domestic cat. Nebelungs have long bodies, wide-set green eyes, long and dense fur, and mild dispositions.[1] The cat is related to the Russian Blue, but with longer, silkier hair, and is in fact sometimes called the Long-haired Russian Blue."
-        good output:
-        "Here's the extracted list of cat breeds:
-        python
-        cat_breeds = ['Nebelung', 'Russian Blue', 'Long-haired Russian Blue']
-        "
+דוגמאות:
+1.
+אם המשפט הוא:        
+        בעבר שרר בלבול באשר למוצאו של גזע זה, ובתחילה הוא נקרא בשם "כחול ארכנגלסק". רק בשנות ה-40 של המאה ה-20 קיבל הגזע את שמו הנוכחי. החתול הרוסי הכחול הפך להיות פופולרי בבריטניה, והחל מ-1912 התקבל גם כגזע מוכר לתערוכות. הרוסי הכחול היה ידוע גם בארצות הברית, והחל משנת 1900 הוא מופיע שם בתערוכות תחת השם "חתול מלטזי".
+
+        
+        
+תוצאה טובה תהיה:
+ ['חתול מלטזי', 'רוסי הכחול', 'כחול ארכנגלסק'']
+  
+  
+2.
+אם המשפט הוא:
+בזמן מלחמת העולם השנייה נכחד הגזע בבריטניה כמעט לחלוטין, וכתוצאה מחוסר בחתולים טהורים מגזע זה השתמשו המגדלים לרבייה גם בחתולים בריטיים קצרי שיער כחולים, ובחתולים סיאמיים, בעלי מאפיינים דומים. 
+תגובה טובה תהיה:
+ ['בריטיים קצרי שיער כחולים', 'חתולים סיאמיים']
+
+  
+3.
+אם המשפט הוא:
+גזעי חתולים עם שיער קצר מגוונים ומרהיבים. כמה מהם כוללים:
+
+    סיאמי - חתולים אלגנטיים וחסוניים עם צבע גוף בהיר ועיניים כחולות.
+    בומביי - חתולים גדול
+
+
+תגובה טובה תהיה:
+ ['סיאמי', 'בומביי']
+
+  
+     
         """
 
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # Ensure the model is correct
+            model="gpt-4",  # Ensure the model is correct
             messages=[
                 {"role": "user", "content": extraction_prompt}
             ],
@@ -72,7 +97,7 @@ def get_cat_breeds_list(text: str):
         )
         # Extract and display the AI's response
         ai_response = response['choices'][0]['message']['content'].strip()
-
+        print(f"{ai_response = }")
         match = re.search(regex, ai_response)
         if match:
             cat_breeds_list = match.group(1)
@@ -83,46 +108,53 @@ def get_cat_breeds_list(text: str):
 
 
 def screen_1():
-    user_prompt = st.text_area("Enter your prompt:")
+    user_prompt = st.text_area("המטרה שלך היא ללמוד על גזעי החתולים השונים, כתבו את הפרומפט שיוציא את המידע הזה")
     evaluation_prompt = f"""
-        Please evaluate the quality of the following prompt for learning about cat breeds, 
-        and provide feedback on how to improve it:\n\n"
-        Prompt: {user_prompt}\n\n
-        Your feedback should include: 
-        1. Whether the prompt is too broad, too short, or too specific. 
-        2. Suggestions for improving the prompt to get a better response.
+    בבקשה תן הערכה של איכות הפרומפט של המשתמש, המטרה של הפרומפט של המשתמש היא להוציא מידע על גזעי חתולים שונים.
+        
+        
+אם הפרומפט של המשתמש מספיק טוב, כתוב Pass בסוף התגובה שלך.
+אבל תכתוב את זה רק אם הפרומפט טוב מספיק לדעתך, אחרת, אל תכתוב Pass
+אם הבעיות בפרומפט של המשתמש הן קטנות, ובסופו של דבר הפרומפט מתמקד בחיפוש מידע על גזעי חתולים ומתאים לחיפוש באמצעות AI, אז כן תכתוב Pass בסוף התגובה שלך.
+
+דוגמאות:
+1.
+אם הפרומפט של המשתמש הוא רק:
+גזעי חתולים
+
+תגובה טובה שלך תהיה:
+הפרומפט דומה מידי לחיפוש בגוגל
 
 
-        If the user's prompt is sufficient please write "Pass" in the end of your response.
-        But, write this "Pass" only if the prompt is good enough.
-        If there are big issues with the user's prompt, don't write this "Pass", 
-        but, if there are only small issues with it, do write "Pass" in the end of your response
+2.
+אם הפרומפט של המשתמש הוא:
+ספר לי של מלחמצ העולם השנייה
 
+תגובה טובה שלך תהיה:
+הפרומפט לא ממוקד בחיפוש מידע על גזעי חתולים
 
+3.
+אם הפרומפט של המשתמש הוא:
+שלום, בבקשה ספר לי על גזעי חתולים שונים, ספציפית אלו עם פרווה קצרה.
 
-        For example,
-        if user_prompt = "Different cat breeds"
-        A good response is:
-        "The prompt is too similar to google search, please try a different prompt"
+תגובה טובה שלך תהיה:
+הפרומפו תקין,
+Pass
 
-        if user_prompt = "Hello, please tell me about World-War II"
-        A good response is:
-        "The prompt isn't requesting information about cat breeds, please try a different prompt"
+        
+        
+הפרומפט של המשתמש:
+        {user_prompt}\n\n
 
-
-
-        if user_prompt = "Hello, please tell me about some various cat breeds"
-        A good response is:
-        "The prompt is good \n Pass"
-        """
+    """
 
     # Button to generate a response
-    if st.button("Generate Response"):
+    if st.button("בדיקת פרומפט"):
         response = evaluate_prompt_with_heuristics(user_prompt)
 
         # if heuristics cancelled it
         if response is not None:
-            st.write("### Heuristic Response:")
+            st.write("### תשובה היוריסטית:")
             st.write(response)
 
         # if the prompt passes heuristics
@@ -130,16 +162,16 @@ def screen_1():
             # Call the OpenAI API to get the response
             try:
                 response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",  # Ensure the model is correct
+                    model="gpt-4",  # Ensure the model is correct
                     messages=[
                         {"role": "user", "content": evaluation_prompt}
                     ],
-                    max_tokens=150,
+                    max_tokens=1500,
                     temperature=0.7,
                 )
                 # Extract and display the AI's response
                 ai_response = response['choices'][0]['message']['content'].strip()
-                st.write("### AI Response:")
+                st.write("### תשובת AI:")
                 st.write(ai_response)
                 prompt_is_good_enough = test_if_prompt_good_enough(user_prompt, ai_response)
                 if prompt_is_good_enough:
@@ -169,16 +201,18 @@ def screen_1():
 
 
 def screen_2():
-    st.write("### AI Response:")
+    st.write("### תשובת AI:")
+    print(f"{st.session_state.ai_response = }")
     st.write(st.session_state.ai_response)
 
-    cat_breed_choices = get_cat_breeds_list(st.session_state.ai_response)
-
+    cat_breed_choices = get_cat_breeds_list(st.session_state.ai_response[::-1])
+    print(f"{cat_breed_choices = }")
+    cat_breed_choices_fixed = [breed[::-1] for breed in eval(cat_breed_choices)]
     option = st.selectbox(
-        "Select the chosen cat breed:",
-        eval(cat_breed_choices))
+        "נא לבחור גזע של חתולים",
+        cat_breed_choices_fixed)
 
-    if st.button("Next"):
+    if st.button("המשך"):
         print(f"{option = }")
         st.session_state.chosen_cat_breed = option
         st.session_state.screen_num = 3
@@ -187,11 +221,11 @@ def screen_2():
 
 def screen3():
     cat_breed = st.session_state.chosen_cat_breed
-    suggested_prompt = f"A serene scene of the {cat_breed} standing on a mountain at sunset with a lake in the background."
+    suggested_prompt = f"  {cat_breed}בסגנון של וינסנט ואן גוך  "
     st.write(suggested_prompt)
-    user_prompt = st.text_area("Enter your prompt:")
+    user_prompt = st.text_area("הכניסו את הפרומפט:")
 
-    if st.button("Generate image"):
+    if st.button("יצירת תמונה"):
         create_image(user_prompt)
 
 
